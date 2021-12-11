5fd65af99b26fe710acc13eb67b89d77252c92ca
shusuke0812 <shu.ota0812@gmail.com>
2021-12-11 18:03:48 +0900

    add: RawRepresentableのサンプルを追加

---
+++ StudyingSwiftPlayground.playground/Sources/Basic/rawrepresentable.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+public enum Color {
+    case primary
+    case sub
+}
+
+extension Color: RawRepresentable {
+    public typealias RawValue = UIColor
+    
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case UIColor.red:   self = .primary
+        case UIColor.blue:  self = .sub
+        default: return nil
+        }
+    }
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .primary:  return UIColor.red
+        case .sub:      return UIColor.blue
+        }
+    }
+}
