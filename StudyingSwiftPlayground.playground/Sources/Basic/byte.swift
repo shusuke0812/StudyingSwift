40b4b942608a64f1e4e40a4bd2f5103ab6de3c74
shusuke0812 <shu.ota0812@gmail.com>
2022-03-31 08:21:10 +0900

    add: N進数型を追加

---
+++ StudyingSwiftPlayground.playground/Sources/Basic/byte.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/**
+ * N進数を使った処理
+ */
+
+struct ByteSample {
+    // リテラル
+    let decimalInt = 17
+    let binaryInt = 0b10001
+    let octalInt = 0o21
+    let hexadecimalInt = 0x11
+}
