c4ac2988046fc4b542154f9cdaa9cbbfc80fd152
shusuke0812 <shu.ota0812@gmail.com>
2022-06-05 16:14:35 +0900

    add: OperationQueueのサンプルコードを追加

--- StudyingSwiftPlayground.playground/Contents.swift
+++ StudyingSwiftPlayground.playground/Contents.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+let operation1 = MyOperation(number: 1)
+let operation2 = MyOperation(number: 2)
+let operation3 = MyOperation(number: 3)
+
+let queue = OperationQueue()
+
+queue.maxConcurrentOperationCount = 1
+
+queue.addOperation(operation1)
+queue.addOperation(operation2)
+
+if operation1.isExecuting {
+    operation2.cancel()
+}
+queue.addOperation(operation3)
--- StudyingSwiftPlayground.playground/Sources/Basic/operation_queue.swift
+++ StudyingSwiftPlayground.playground/Sources/Basic/operation_queue.swift
@@ -1,3 +1,10 @@
+/**
+ * Doc
+ * - WWDC：https://developer.apple.com/videos/play/wwdc2015/226/
+ * - 参考１：https://www.macneko.com/entry/cancel-operation-in-operationqueue
+ * - 参考２：https://tiny-wing.hatenablog.com/category/swift?page=1449738361
+ */
+
 import Foundation
 
 open class MyOperation: Operation {
@@ -8,7 +15,32 @@ open class MyOperation: Operation {
     }
     
     open override func main() {
-        Thread.sleep(forTimeInterval: 1)
-        print(number)
+        if isCancelled {
+            return
+        }
+        sleep(2)
+        print("number = \(number), \(Date().timeIntervalSince1970)")
     }
 }
+
+// MARK: - Source
+/*
+ 
+import Foundation
+
+let operation1 = MyOperation(number: 1)
+let operation2 = MyOperation(number: 2)
+let operation3 = MyOperation(number: 3)
+
+let queue = OperationQueue()
+
+queue.maxConcurrentOperationCount = 1
+
+queue.addOperation(operation1)
+queue.addOperation(operation2)
+
+if operation1.isExecuting {
+    operation2.cancel()
+}
+queue.addOperation(operation3)
+*/
