b3ee486c1d1969a83eb621dc8101b74cd9cfbfe8
shusuke0812 <shu.ota0812@gmail.com>
2022-06-05 16:32:26 +0900

    fix: operation queue にコメントとサンプルコードを追加

--- StudyingSwiftPlayground.playground/Sources/Basic/operation_queue.swift
+++ StudyingSwiftPlayground.playground/Sources/Basic/operation_queue.swift
@@ -31,13 +31,20 @@ import Foundation
 let operation1 = MyOperation(number: 1)
 let operation2 = MyOperation(number: 2)
 let operation3 = MyOperation(number: 3)
+ 
+// タスクが終了・キャンセルしたときに呼ばれる
+operation1.completionBlock = {
+    print("operation1 runs completion")
+}
 
 let queue = OperationQueue()
+let mainQueue = OperationQueue.main // MainThreadで同期実行
 
-queue.maxConcurrentOperationCount = 1
+queue.maxConcurrentOperationCount = 1　// スレッド実行数, `1`の場合は同期処理
 
 queue.addOperation(operation1)
 queue.addOperation(operation2)
+// 補足：operation1.addOperation(operation3)とすると`operation3`の終了を待ってから`operation1`が実行される。1が3に依存している。
 
 if operation1.isExecuting {
     operation2.cancel()
